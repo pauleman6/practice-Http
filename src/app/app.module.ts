027ecbdcfa3dbfdb1ee259b73394beb2e80ddbcc
pauleman6 <pauleman6@gmail.com>
2020-04-04 15:52:24 -0500

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeService } from './employee.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, EmployeeListComponent, EmployeeDetailComponent ],
+  declarations: [ AppComponent, EmployeeListComponent, EmployeeDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers: [EmployeeService]
 })
